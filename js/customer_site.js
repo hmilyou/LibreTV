9b5b2a87e042ddc949264ada55b5aace63fd8aa3
hmilyou <44571216+hmilyou@users.noreply.github.com>
2025-10-03 15:20:05 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,18 +2,10 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.hongniuzy2.com/api.php/provide/vod',
         name: '红牛',
-        api: 'https://cj.lziapi.com/api.php/provide/vod',
-        name: '影视工程',
-        api: 'https://360zyzz.com/api.php/provide/vod',
-        name: '360',
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+    }
+    hongniu: {
+        api: 'https://www.hongniuzy2.com/api.php/provide/vod',
+        name: '红牛',
     }
 };
 
